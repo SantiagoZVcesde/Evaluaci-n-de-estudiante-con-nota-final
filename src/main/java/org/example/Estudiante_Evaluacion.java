0960175c1d8e874016244b1668297196e8df5076
SantiagoZVcesde <zapatavillasantiago@gmail.com>
2025-05-08 00:23:26 -0500

    se agrengan unos cambios

--- src/main/java/org/example/Estudiante_Evaluacion.java
+++ src/main/java/org/example/Estudiante_Evaluacion.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Estudiante_Evaluacion {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         double Nota_uno = 0;
         double Nota_dos = 0;
         double Nota_tres = 0;
@@ -21,6 +22,7 @@ public class Estudiante_Evaluacion {
 
         double Nota_Mas_Alta = 0;
         double Nota_Mas_Baja = 0;
+
         System.out.println("Ingrese el valor de Nota uno de 1 a 5");
         Nota_uno = sc.nextDouble();
         System.out.println("Ingrese el porcentaje de la nota uno");
@@ -41,8 +43,26 @@ public class Estudiante_Evaluacion {
         Nota_Final = (Nota_uno * Porcenataje_Nota_uno/100) + (Nota_dos * Porcenataje_Nota_dos/100) + (Nota_tres * Porcenataje_Nota_tres/100) + (Nota_cuatro * Porcenataje_Nota_cuatro/100);
         Promedio = Nota_uno + Nota_dos + Nota_tres + Nota_cuatro / 4 ;
 
-        Nota_Mas_Alta = Math.max(Math.max(Nota_uno, Nota_dos), Math.max(Nota_tres, Nota_cuatro));
-        Nota_Mas_Baja = Math.min(Math.min(Nota_uno, Nota_dos), Math.min(Nota_tres, Nota_cuatro));
+        if (Nota_uno >= Nota_dos && Nota_uno >= Nota_tres && Nota_uno >= Nota_cuatro) {
+            Nota_Mas_Alta = Nota_uno;
+        } else if (Nota_dos >= Nota_tres && Nota_dos >= Nota_cuatro) {
+            Nota_Mas_Alta = Nota_dos;
+        } else if (Nota_tres >= Nota_cuatro) {
+            Nota_Mas_Alta = Nota_tres;
+        } else {
+            Nota_Mas_Alta = Nota_cuatro;
+        }
+
+        if (Nota_uno <= Nota_dos && Nota_uno <= Nota_tres && Nota_uno <= Nota_cuatro) {
+            Nota_Mas_Baja = Nota_uno;
+        } else if (Nota_dos <= Nota_tres && Nota_dos <= Nota_cuatro) {
+            Nota_Mas_Baja = Nota_dos;
+        } else if (Nota_tres <= Nota_cuatro) {
+            Nota_Mas_Baja = Nota_tres;
+        } else {
+            Nota_Mas_Baja = Nota_cuatro;
+        }
+
         double diferencia = Nota_Mas_Alta - Nota_Mas_Baja;
 
         System.out.println("La nota mas alta es " + Nota_Mas_Alta);
